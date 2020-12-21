4f5103f72f8d1713b5cfda5f4c730645809ffccf
Atharva Agashe <ag4b.uk@gmail.com>
2020-12-21 15:16:43 +0530

    added division

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -11,7 +11,13 @@ public class Calc {
 		System.out.println(add(10, 5));
 		System.out.println(subs(20,10));
 		System.out.println(prod(20,10));
+		System.out.println(div(20,10));
 		
+	}
+
+	private static int div(int i, int j) {
+		// TODO Auto-generated method stub
+		return i/j;
 	}
 
 	private static int prod(int i, int j) {
