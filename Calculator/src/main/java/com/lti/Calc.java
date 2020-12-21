b4540ce25020bc5ff3c9d0888d6ce5cd604b8f00
Atharva Agashe <ag4b.uk@gmail.com>
2020-12-21 15:09:38 +0530

    added pull

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -10,7 +10,13 @@ public class Calc {
 		// TODO Auto-generated method stub
 		System.out.println(add(10, 5));
 		System.out.println(subs(20,10));
+		System.out.println(prod(20,10));
 		
+	}
+
+	private static int prod(int i, int j) {
+		// TODO Auto-generated method stub
+		return i*j;
 	}
 
 	private static int subs(int i, int j) {
