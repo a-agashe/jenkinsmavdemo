94bfd978710b93ca4ffb159061d575dba9d60db4
Atharva Agashe <ag4b.uk@gmail.com>
2020-12-21 12:43:38 +0530

    jenkins demo calc

---
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -0,0 +1,14 @@
+package com.lti;
+
+public class Calc {
+	private static int add(int i, int j) {
+		// TODO Auto-generated method stub
+		return (i + j);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(10, 5));;
+	}
+
+}
